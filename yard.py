eaf2363a1af5d5f984d4e9169b7804810188a49c
jeonnamoo <jnamoo2002@gmail.com>
2024-11-18 12:21:22 +0900

    change yard code detail

--- yard.py
+++ yard.py
@@ -1,41 +1,43 @@
 from pico2d import *
-
 import game_framework
 import game_world
 from door import Door
 from girl import Girl
 import livingroom
 
-
 image = None
 door = None
 door_x, door_y = 720, 550  # 문 위치
 width, height = 1440, 960  # Yard 크기
 girl = None
+last_used_door = None
 
 def init():
-    global image, door, girl
-    image = load_image('yard.png')  # 배경 이미지 로드
-    door = Door(width=32, height=32)  # 문 크기 설정
+    global image, door, girl, last_used_door
+    image = load_image('yard.png')
+    door = Door()
 
-    girl = game_world.get_object_by_class(Girl)  # 기존 girl 객체 가져오기
-    if not girl:  # girl 객체가 없으면 새로 생성
+    girl = game_world.get_object_by_class(Girl)
+    if not girl:
         girl = Girl()
         game_world.add_object(girl, 2)
 
-    girl.x, girl.y = 720, 550  # 초기 위치
-
+    # 마지막 사용된 문에 따라 초기 위치 설정
+    if last_used_door == 'door1':
+        girl.x, girl.y = 720, 550
+    else:  # 기본 위치
+        girl.x, girl.y = 500, 500
 
 def draw():
     global image, door
     clear_canvas()
-    image.draw_to_origin(0, 0, width, height)  # 배경 그리기
-    door.draw(door_x, door_y)  # 문 그리기
+    image.draw_to_origin(0, 0, width, height)
+    door.draw(door_x, door_y)
     game_world.render()
     update_canvas()
 
 def handle_events():
-    global girl, door
+    global girl, last_used_door
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -43,25 +45,21 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-            # Girl과 Door 사이의 거리 계산
             distance = ((girl.x - door_x) ** 2 + (girl.y - door_y) ** 2) ** 0.5
-            if distance <= 30:  # 문 근처(거리 50 이하)일 때만 Livingroom으로 전환
+            if distance <= 30:
+                last_used_door = 'door1'
                 game_framework.change_mode(livingroom)
         else:
             if girl:
                 girl.handle_event(event)
 
 def update():
-    global girl
-    if girl:  # girl 객체가 존재할 경우에만 실행
-        girl.x = max(500, min(820, girl.x))  # x축 이동 범위 제한
-        girl.y = max(475, min(530, girl.y))  # y축 이동 범위 제한
-    game_world.update()  # 다른 객체들도 업데이트
+    if girl:
+        girl.x = max(500, min(820, girl.x))
+        girl.y = max(475, min(530, girl.y))
+    game_world.update()
 
-def pause(): pass
-def resume(): pass
 def finish():
     global image, door
     del image
     del door
-
