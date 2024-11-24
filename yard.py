bc742aff16d3718c1eb8f73edfa710a60ee269a0
jeonnamoo <jnamoo2002@gmail.com>
2024-11-24 15:54:35 +0900

    girl move broom

--- yard.py
+++ yard.py
@@ -16,11 +16,14 @@ width, height = 1440, 960  # Yard 크기
 broom_x, broom_y = 520, 490
 girl = None
 
+broom_attached = False
+
+
 def init():
-    global image, door, girl, broom
+    global image, door, girl, broom, broom_attached
     image = load_image('yard.png')  # 배경 이미지 로드
     door = Door(width=32, height=32)  # 문 크기 설정
-    broom = Broom(width=32, height=32)  # 문 크기 설정
+    broom = Broom(width=32, height=32)  # broom 크기 설정
 
     girl = game_world.get_object_by_class(Girl)  # 기존 girl 객체 가져오기
     if not girl:  # girl 객체가 없으면 새로 생성
@@ -29,19 +32,23 @@ def init():
 
     girl.x, girl.y = 720, 550  # 초기 위치
 
-
+    broom_attached = False
 
 def draw():
-    global image, door, broom
+    global image, door, broom, broom_attached
     clear_canvas()
     image.draw_to_origin(0, 0, width, height)  # 배경 그리기
     door.draw(door_x, door_y)  # 문 그리기
-    broom.draw(broom_x, broom_y)
+    if not broom_attached:
+        broom.draw(broom_x, broom_y)
     game_world.render()
+    if broom_attached:
+        broom.draw(girl.x, girl.y)
+
     update_canvas()
 
 def handle_events():
-    global girl, door
+    global girl, door, broom, broom_attached
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -53,6 +60,10 @@ def handle_events():
             distance = ((girl.x - door_x) ** 2 + (girl.y - door_y) ** 2) ** 0.5
             if distance <= 30:  # 문 근처(거리 50 이하)일 때만 Livingroom으로 전환
                 game_framework.change_mode(livingroom)
+            distance_to_broom = ((girl.x - broom_x) ** 2 + (girl.y - broom_y) ** 2) ** 0.5
+            if distance_to_broom <= 30 and not broom_attached:  # broom 근처에서 space를 눌렀을 때
+                broom_attached = True  # broom이 girl에 붙음
+
         else:
             if girl:
                 girl.handle_event(event)
